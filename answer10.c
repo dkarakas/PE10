0428872ab74954b6b6462115a7597247c2884a33
Dimcho Karakashev <dkarakas@purdue.edu>
2016-04-03 00:52:03 -0400

    creating only linked list

--- answer10.c
+++ answer10.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include"answer10.h"
+
+lnode * n_construct(long int ch, int weight);
+lnode * Enqueue(lnode * * head,long int ch, int weight);
+void destroy_list(lnode* head);
+void print_list(lnode *head);
 
 void print_weight(FILE*fptr,FILE* out_fptr,long int*weight){
   //VARIABLES
@@ -21,9 +27,81 @@ void print_weight(FILE*fptr,FILE* out_fptr,long int*weight){
 }
 
 void priority_queue_by_weight(FILE*fptr, long int*weight){
-  //
+  lnode *head;
+  lnode *new_node =NULL;
+  int i;
+  for(i = 0; i <256; i++){
+    if(weight[i] != 0){
+      new_node = Enqueue(&head,i,weight[i]); 
+    }
+  }
 
-
-printf("chec");
-
+  print_list(head);
+  destroy_list(head);
 }
+
+lnode* n_construct(long int ch,int weight){
+  lnode* new_node = (lnode *)malloc(sizeof(lnode));
+  if(new_node == NULL){
+    fprintf(stderr,"failed to malloc new node");
+    return NULL;
+  }
+  new_node->weight = weight;
+  new_node->ch = (char)ch;
+  new_node->l_node = NULL;
+  new_node->r_node = NULL;
+  return new_node;
+} 
+
+lnode* Enqueue(lnode **head,long int ch, int weight){
+  lnode* new_node = n_construct(ch,weight);
+  if(new_node == NULL){
+    fprintf(stderr,"failed to malloc new node");
+    return NULL;
+  }
+  lnode dummy;
+  lnode* prev = &dummy;
+  lnode* cur = *head;
+  dummy.r_node = *head;
+  //printf(stderr,"check");
+  while(cur != NULL){
+    if(cur->weight > new_node->weight)
+      break;
+    else if(cur->weight == new_node->weight){
+      if(new_node->weight == 0)
+        break;
+      if(strcmp((char *)(&cur->ch),(char *)(&prev->ch)) > 0)
+        break;
+    }else{
+      prev = cur;
+      cur = cur->r_node;
+    }
+  }
+  *head = dummy.r_node;
+  new_node->r_node = cur;
+  prev->r_node = new_node;
+  return new_node;
+}
+
+
+void destroy_list(lnode *head){
+  lnode *temp = head;
+  while(head != NULL){
+    temp = head->r_node;
+    head->r_node = NULL;
+    free(head);
+    head = temp;
+  }
+  
+}
+
+
+void print_list(lnode *head){
+  while(head != NULL){
+    printf("%c",head->ch);
+    fprintf(stdout, "->");
+    head = head->r_node;
+  } 
+  fprintf(stdout, "NULL\n");
+}
+
--- answer10.h
+++ answer10.h
@@ -1,19 +1,15 @@
 #ifndef PE10_H
 #define PE10_H
 
- typedef struct _node {
+
+ typedef struct _lnode {
    int weight;
    char ch;
-   struct node* l_node;
-   struct node* r_node; 
- }node; 
+   struct _lnode* l_node;
+   struct _lnode* r_node; 
+ }lnode; 
 
  void print_weight(FILE*file,FILE*output,long int*weight);
  void priority_queue_by_weight(FILE*file, long int*weight);
-
-
-
-
-
 
 #endif
